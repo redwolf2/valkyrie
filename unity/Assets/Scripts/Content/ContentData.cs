5f7c65bfc815fac8341f7a0b57cc2f9bd1743cea
redwolf <redwolf2@gmx.de>
2019-01-03 02:37:01 +0100

    Removed more quest generator parts

--- unity/Assets/Scripts/Content/ContentData.cs
+++ unity/Assets/Scripts/Content/ContentData.cs
@@ -428,7 +428,6 @@ public class ContentData {
             else if (tileSides[name].priority == d.priority)
             {
                 tileSides[name].sets.Add(packID);
-                tileSides[name].count += d.count;
             }
         }
 
@@ -698,7 +697,6 @@ public class ContentData {
             else if (tokens[name].priority == d.priority)
             {
                 tokens[name].sets.Add(packID);
-                tokens[name].count += d.count;
             }
         }
 
@@ -1106,9 +1104,6 @@ public class TileSideData : GenericData
     public float aspect = 0;
     public string reverse = "";
     public static new string type = "TileSide";
-    public List<string> map = new List<string>();
-    public Vector2 mapZero = new Vector2(0, 0);
-    public int count = 1;
 
     public TileSideData(string name, Dictionary<string, string> content, string path) : base(name, content, path, type)
     {
@@ -1150,42 +1145,6 @@ public class TileSideData : GenericData
         if (content.ContainsKey("reverse"))
         {
             reverse = content["reverse"];
-        }
-
-        // How many of these tiles are available?
-        if (content.ContainsKey("count"))
-        {
-            int.TryParse(content["count"], out count);
-        }
-
-        // Get the tile map if available
-        for (int mapLine = 0; mapLine < 100; mapLine++)
-        {
-            if (content.ContainsKey("map0" + mapLine))
-            {
-                map.Add(content["map0" + mapLine]);
-            }
-            else if (content.ContainsKey("map" + mapLine))
-            {
-                map.Add(content["map" + mapLine]);
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        // Get the tile map 0 location
-        if (content.ContainsKey("zeropos"))
-        {
-            string xString = content["zeropos"].Substring(0, content["zeropos"].IndexOf(','));
-            string yString = content["zeropos"].Substring(content["zeropos"].IndexOf(',') + 1);
-            int xZeroInt = 0;
-            int.TryParse(xString, out xZeroInt);
-            int yZeroInt = 0;
-            int.TryParse(xString, out yZeroInt);
-            mapZero.x = xZeroInt;
-            mapZero.y = yZeroInt;
         }
     }
 }
@@ -1454,7 +1413,6 @@ public class TokenData : GenericData
     // 0 means token is 1 square big
     public float pxPerSquare = 0;
     public static new string type = "Token";
-    public int count = 1;
 
     public TokenData(string name, Dictionary<string, string> content, string path) : base(name, content, path, type)
     {
@@ -1490,12 +1448,6 @@ public class TokenData : GenericData
         if (content.ContainsKey("pps"))
         {
             float.TryParse(content["pps"], out pxPerSquare);
-        }
-
-        // How many of these tokens are available?
-        if (content.ContainsKey("count"))
-        {
-            int.TryParse(content["count"], out count);
         }
     }
 
